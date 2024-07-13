480db42edf97cc9ec124c6b26e4cff78046ab6e6
Jaehun Kim <jaehukim@c3r12s2.42gyeongsan.kr>
2024-07-13 20:33:10 +0900

    [Build] proj started

---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaehukim <jaehukim42@student.42gyeong      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/13 20:27:16 by jaehukim          #+#    #+#             */
+/*   Updated: 2024/07/13 20:31:42 by jaehukim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+
+# endif
